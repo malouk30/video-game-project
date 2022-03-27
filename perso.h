7292f8c18270d788b41a58b7919991edbde979f4
malouk30 <100469136+malouk30@users.noreply.github.com>
2022-03-27 16:27:06 +0200

    Update perso.h

--- perso.h
+++ perso.h
@@ -1,3 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
 typedef struct
 {
     int status,num,dr,vi;
