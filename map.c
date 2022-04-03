8c361824b5668fa10ac6d4e8b5c2204f20b73888
baha-elfidha <100535387+baha-elfidha@users.noreply.github.com>
2022-04-03 23:21:15 +0000

    Update map.c

--- map.c
+++ map.c
@@ -46,3 +46,69 @@ void afficherminimap(minimap m, SDL_Surface *screen)
 {
     SDL_BlitSurface(m.map, NULL, screen, &m.pos_map);
 }
+void initialiser_temps(temps *t)
+{
+t->texte = NULL;
+
+t->position.x=0;
+t->position.y=0;
+
+t->police = NULL;
+t->police = TTF_OpenFont("ka1.ttf", 20);
+if(t->police==NULL)
+printf("error police\n");
+strcpy( t->entree,"");    
+t->tempsActuel = 0; 
+t->tempsPrecedent = 0;
+t->min = 0;
+t->cmp=0;
+
+}
+void afficher_temps(temps *t,SDL_Surface *screen)
+{
+
+ int continuer = 1;
+
+TTF_Font *police = NULL;//
+
+
+ SDL_Event event;
+ SDL_Color couleurNoire = {0, 0, 0}, couleurRouge = {255, 0, 0};
+ 
+t->tempsActuel = SDL_GetTicks();
+ 
+sprintf(t->entree, "Temps : %d",t->cmp);
+
+
+t->texte = TTF_RenderText_Shaded(t->police,t->entree, couleurNoire, couleurRouge);  
+
+
+t->tempsActuel = SDL_GetTicks();
+
+ 
+     
+        if (t->tempsActuel - t->tempsPrecedent >= 1000) 
+         {
+            t->cmp++;  
+ 
+            if(t->cmp ==60)
+             {
+               t->cmp=0;
+               t->cmp++;
+                  t->min++;
+             }
+  sprintf(t->entree, ":%dm :%ds",t->min,t->cmp); 
+ 
+
+ t->texte = TTF_RenderText_Shaded(t->police, t->entree, couleurNoire, couleurRouge);
+      t->tempsPrecedent = t->tempsActuel;  
+          }
+    
+        t->position.x = 800;
+        t->position.y = 0;
+        SDL_BlitSurface(t->texte, NULL, screen, &t->position);
+	
+        SDL_Flip(screen);
+  
+
+}
