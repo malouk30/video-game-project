d5dfac411de399cf4416a69382258fdae8f53d41
malouk30 <100469136+malouk30@users.noreply.github.com>
2022-04-04 07:47:51 +0200

    Update enigmetf.c

--- enigmetf.c
+++ enigmetf.c
@@ -1,338 +1,104 @@
 #include "enigmetf.h"
-void InitEnigme(enigmetf *e, char *nomfichier[ ])
+void init_enigme( enigme *e ,char *nomfichier)
 {
-nomfichier [100] ;
+   e->background= IMG_Load("img/background.png") ;
+   e->button= IMG_Load("img/button.png");
+   e->button_s= IMG_Load("img/button_s.png");
 
-FILE *fquestion=NULL;
-FILE *freponse=NULL;
-FILE *fvraireponse=NULL;
+	TTF_Init( ) ;
+	SDL_Color couleur = {0,0,0};
 
-e->pos_selected=0;
-e->background= IMG_Load("img/background.png") ;
-e->button= IMG_Load("img/button.png");
-e->button_s= IMG_Load("img/button_s.png");
+(*e) . police = TTF_OpenFont("arial. ttf", 30);
 
-TTF_Init( );
-
-SDL_Color couleur = {0,0,0};
-
-(*e).police = TTF_OpenFont("arial. ttf", 30);
+   FILE *fquestion=NULL;
+    e->question = NULL;
+    e->pos_question.x=200;
+    e->pos_question.y=250;
+    
 e->police1 = TTF_OpenFont("arial. ttf", 20);
 
-char questionn [50];
+   FILE *freponse=NULL;
+    e->pos_reponse_1.x=200;
+    e->pos_reponse_1.y=500;
+    e->reponse_2 = NULL;
+    e->pos_reponse_2.x=450;
+    e->pos_reponse_2.y=500;
+    e->reponse_3 =NULL;
+    e->pos_reponse_3.x=700;
+    e->pos_reponse_3.y=500;
 
-srand (time (NULL)) ;
-e->num_question=rand ( )%4;
+    e->alea = 0;
 
-while(e->num_question==0) {
-	srand ( time ( NULL) ) ;
-	e->num_question=rand( )%4;
+    e->youwin =IMG_Load("img/youwin.png");
+    e->youlost =IMG_Load("img/youlost.png");
+    e->position_youwin.x = 170;
+    e->position_youwin.y = 255;
+    e->position_youlost.x = 240;
+    e->position_youlost.y = 255;
 }
-sprintf(*nomfichier , "questions . txt");
-fquestion=fopen(*nomfichier, "r+");
 
-if(fquestion! =NULL) {
-	int test=1 ;
-while (fgets(questionn, 50, fquestion) &&(test!=e->num_question)) {
-	test+ + ;
-}
-fclose(fquestion);
-}
-
-char reponse [20];
-
-int min, max;
-switch (e->num_question)
+void blit_enigme ( enigme e, SDL_Surface* screen)
 {
-case 1:
+        SDL_BlitSurface(en->question,NULL,screen,&en->pos_question);
+        SDL_BlitSurface(en->reponse_1,NULL,screen,&en->pos_reponse_1);
+        SDL_BlitSurface(en->reponse_2,NULL,screen,&en->pos_reponse_2);
+        SDL_BlitSurface(en->reponse_3,NULL,screen,&en->pos_reponse_3);
+
+}
+
+void free_enigme( enigme *e)
 {
-	min=0;
-	max=2;
-break;
+    SDL_FreeSurface(e->question);
+    SDL_FreeSurface(e->reponse_1);
+    SDL_FreeSurface(e->reponse_2);
+    SDL_FreeSurface(e->reponse_3);
+    SDL_FreeSurface(e->youwin);
+    SDL_FreeSurface(e->youlost);
+    TTF_CloseFont(e->police);
+    TTF_CloseFont(e->police1);
 }
-case 2:
+
+void alea_enigme(enigme *e)
 {
-	min=3;
-	max=5;
-break;
-}
-Case 3:
-	min=6;
-	max=8;
-break ;
-}
-}
-	freponse=fopen( "reponses.txt", "r+");
-if (freponse!=NULL) {
-	int test=0;
-While (fgets((reponse), 15, freponse) &&(test<max) ) { //parcour
+	char ques[20] = {0};
+	char rep1[10] = {0};
+	char rep2[10] = {0};
+	char rep3[10] = {0};
+int c_question;
+int c_reponses;
+int ligne_quest =1;
+int ligne_rep=1;
+	srand(time(NULL));
+	do {
+		e->alea = rand()%3 ;
+	}while ((e->alea >=1)&&(e->alea <4));
 
-if(test>=min) 
-{
-	e->reponses [test-min]=TTF_RenderText_Blended(e->police1, reponse, couleur);
-}
-  	 test++;
-}
-fclose(freponse);
-}
-	srand( time ( NULL ) ) ;
-	e->positionVraiReponse=rand ( )%4;
+	freponse=fopen( "reponses. txt", "r+");
+	fquestion = fopen("question.txt","r+"); 
+	if ( freponse!=NULL )
+	{
+		while (ligne_quest < e->alea)
+		{
+			c_question = fgetc(fquestion);
+			if ( c_question == '\n')
+				ligne_quest++;
+		}
+		fgets(question,20,fquestion);
+		fclose(fquestion);
 
-while(e->positionVraiReponse==0) {
-	srand (time (NULL)) ;
-	e->positionVraiReponse=rand( )%4;
-}
-printf("%d\n", e->positionVraiReponse);
-
-	e->question = TTF_RenderText_Blended(e->police, questionn, couleur);
-	
-	e->pos_question . x=270;
-	e->pos_question . y=270;
-
-	e->pos_reponse1. x=375;
-	e->pos_reponse1. y=522;
-
-	e->pos_reponse2. x=177;
-	e->pos_reponse2. y=648;
-
-	e->pos_reponse3 . x=566;
-	e->pos_reponse3. y=651;
-
-	e->pos_reponse1txt. x=e->pos_reponse1. x+50;
-	e->pos_reponse1txt. y=522+30;
-
-	e->pos_reponse2txt . x=177+50;
-	e->pos_reponse2txt. y=648+30;
-
-	e->pos_reponse3txt . x=566+50;
-	e->pos_reponse3txt . y=651+30;
-}
-
-void afficherEnigme (enigmatf e, SDL_Surface *ecran)
-{
-	SDL_BlitSurface(e. background, NULL, ecran, NULL);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. question, NULL, ecran, &e.pos_question);
-	SDL_Flip(ecran);
-
-switch(e. positionVraiReponse) {
-case 1:{
-	switch (e.pos_selected) {
-case 1 :
-{
-	SDL_Blitsurface(e. button_s, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran);
-	SDL_Blitsurface(e.button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran);
-	SDL_Blitsurface(e.button, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e.pos_reponse1txt);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e . pos_reponse2txt) ;
-	SDL_Flip(ecran);
-	SDL_Blitsurface(e. reponses[1], NULL, ecran, &e.pos_reponse3txt);
-	SDL_Flip(ecran) ;
-}
-break ;
-case 2 :
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button_s, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse3) ;
-	SDL_Flip(ecran);
-	
-	SDL_Blitsurface(e. reponses [2], NULL, ecran, &e.pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_Blitsurface(e. reponses [1], NULL, ecran, &e.pos_reponse3txt);
-	SDL_Flip(ecran);
-}
-break ;
-case 3:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran) ;
-	SDL_Blitsurface(e. button_s, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e. pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_Blitsurface(e. reponses[1], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran);
-}
-break;
-default:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse3);
-	
-	SDL_Flip(pran) ;
-	SDL_BlitSurface(e. reponses [2], NULL, ecran, &e . pos_reponse1txt);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. reponses [0], NULL, ecran, &e.pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses [ 1], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran);
-}
-}
-break;
-}
-case 2:
-{
-switch (e.pos_selected) {
-case 1:
-{
-	SDL_BlitSurface(e. button_s, NULL, ecran, &e. pos_reponse1) ;
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran);
-	SDL_Blitsurface(e.button, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e.pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e. pos_reponse2txt);
-	SDL Blitsurface (e. reponses[1], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran);
-}
-break ;
-case 2:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran);
-	SDL Blitsurface ( e.button_s, NULL, ecran, &e.pos_reponse2);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e.button, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e. pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL BlitSurface(e. reponses[1], NULL, ecran, &e.pos_reponse3txt);
-	SDL_Flip(ecran);
-}
-break
-case 3:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e.pos_reponse1);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e.pos_reponse2);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button_s, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran) ;
-
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e. pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses [2], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[1], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran);
-}
-break;
-default:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran) ;
-
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e.pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses [2], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_Blitsurface(e. reponses [1], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran) ;
-}
-break;
-}
-case 3:
-{
-switch(e.pos_selected) {
-case 1 :
-{
-	SDL_BlitSurface(e. button_s, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. button, NULL, ecran, &e.pos_reponsez);
-	SDL_Flip(elcran);
-	SDL_BlitSurface(e. button, NULL, ecran, &e.pos_reponse3);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e. pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses [1], NULL, ecran, &e . pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses [2], NULL, ecran, &e . pos_reponse3txt);
-	SDL_Flip(ecran) ;
-}
-break;
-case 2 :
-{
-	SDL_Blitsurface(e. button, NULL, ecran, &e.pos_reponse1);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. button_s, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran) ;
-
-	SDL_BlitSurface(e. reponses[0] NULL, ecran, &e. pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[1], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran) ;
-}
-break;
-Case 3:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. button_s, NULL, ecran, &e . pos_reponse3);
-	SDL_Flip(ecran) ;
-
-	SDL_BlitSurface(e. reponses[0] NULL, ecran, &e. pos_reponse1txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[1], NULL, ecran, &e. pos_reponse2txt);
-	SDL_Flip(ecran) ;
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran) ;
-}
-break;
-default:
-{
-	SDL_BlitSurface(e. button, NULL, ecran, &e. pos_reponse1);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e.button, NULL, ecran, &e. pos_reponse2);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e.button, NULL, ecran, &e. pos_reponse3);
-	SDL_Flip(ecran);
-
-	SDL_BlitSurface(e. reponses[0], NULL, ecran, &e. pos_reponseltxt);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. reponses[1], NULL, ecran, &e.pos_reponse2txt);
-	SDL_Flip(ecran);
-	SDL_BlitSurface(e. reponses[2], NULL, ecran, &e. pos_reponse3txt);
-	SDL_Flip(ecran); 
-}
-}
-break ;
-}
+		while ( ligne_reponse < e->alea )
+		{
+			c_reponses = fgetc(freponse);
+			if ( c_reponses == '\n')
+				ligne_reponse++;
+		}
+		fscanf(freponse,"%s %s %s",rep1,rep2,rep3);
+		fclose(freponse);
+	}
+                 e->question     = TTF_RenderText_Blended(e->police,ques,Color);
+	e->reponse_1 = TTF_RenderText_Blended(e->police1,rep1,Color);
+	e->reponse_2 = TTF_RenderText_Blended(e->police1,rep2,Color);
+	e->reponse_3 = TTF_RenderText_Blended(e->police1,rep3,Color);
 }
 SDL_Delay(100);
 }
